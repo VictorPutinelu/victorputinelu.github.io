f36535ee2cd011a76c945cba6dc826691b58b5ed
VictorPutinelu <victorputinelu95@gmail.com>
2016-02-07 21:18:42 +0100

    Header uguale per tutte le pagine

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+	<div class="container">		
+		<div class="logo">
+			<h1><a href="#">Pulito.it</a></h1>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contacts</a></li>
+				<li><a href="#"></a></li>
+			</ul>
+		</nav>
+	</div>	
+</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<?php include_once('header.php'); ?>
+		<div class="container">		
+			p>lorem500
+		</div>
+		
+		<footer>
+			All Rights are reserved
+		</footer>
+	</body>
+</html>
